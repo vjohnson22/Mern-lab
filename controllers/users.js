f215efa2f4a01c33f322a28106c8d49073ff1726
Victor Johnson <vjohnson22@gmail.com>
2019-08-16 10:14:26 -0400

    login added

--- controllers/users.js
+++ controllers/users.js
@@ -38,5 +38,31 @@ router.post('/signup', (req, res) => {
     }
   })
 
+  router.post('/login', (req, res) => {
+    if (req.body.email && req.body.password) {
+      User.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+          if (user.password === req.body.password) {
+            var payload = {
+              id: user.id
+            }
+            var token = jwt.encode(payload, config.jwtSecret)
+            res.json({
+              token: token
+            })
+          } else {
+            res.sendStatus(401)
+          }
+        } else {
+          res.sendStatus(401)
+        }
+      })
+    } else {
+      res.sendStatus(401)
+    }
+  })
+  
+
+
 
 module.exports = router
