e2111dec429c2d2c4c055048a30ec97f2b3890aa
Victor Johnson <vjohnson22@gmail.com>
2019-08-16 10:11:57 -0400

    sign up added

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require('express')
 const app = express()
 const parser = require('body-parser')
 const cors = require('cors')
+const passport = require('./config/passport')()
+const userController = require('./controllers/users.js')
+
 
 app.use(cors())
 app.use(parser.json())
 
 
 app.use('/', require('./routes/episodes'))
-
+app.use(passport.initialize())
+app.use('/users', userController)
 
 app.set("port", process.env.PORT || 7000);
 
